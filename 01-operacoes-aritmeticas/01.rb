0d414a9a06659aa1f84082d9a851f18fec8d65b1
Fernando Wagner <fwmpa2@gmail.com>
2023-04-22 13:57:14 -0300

    Small tweaks
    Signed-off-by: Fernando Wagner <fwmpa2@gmail.com>

--- 01-operacoes-aritmeticas/01.rb
+++ 01-operacoes-aritmeticas/01.rb
@@ -1,4 +1,4 @@
 puts "Digite um numero:" 
 number = gets.chomp.to_i
 
-p("O sucessor de #{number} é #{number+1}, e o antecessor é #{number -1}.")
+p("O sucessor de #{number} é #{number + 1}, e o antecessor é #{number - 1}.")
