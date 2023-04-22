c3997bc6c00b3e2dd1df15f29d9264646ca57c46
Fernando Wagner <fwmpa2@gmail.com>
2023-04-22 13:55:22 -0300

    Small tweaks
    Signed-off-by: Fernando Wagner <fwmpa2@gmail.com>

--- 01-operacoes-aritmeticas/06.rb
+++ 01-operacoes-aritmeticas/06.rb
@@ -2,7 +2,6 @@ puts("Digite a distancia em km: ")
 distance = gets.chomp.to_i
 puts("Digite o preço do combustivel: ")
 gas_price = gets.chomp.to_f
-p distance
 
 gas_litters = distance / 16.0
 total_gas_price = (gas_litters * gas_price)
